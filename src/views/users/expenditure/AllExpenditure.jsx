0bb687b6c4588ab83411cbbca679945465103f41
blessme247 <solabiabraham@gmail.com>
2024-02-12 21:39:20 +0100

    Completed view income and view expenditure

--- src/views/users/expenditure/AllExpenditure.jsx
+++ src/views/users/expenditure/AllExpenditure.jsx
@@ -34,7 +34,7 @@ import CustomTextField from 'src/@core/components/mui/text-field'
 // ** Styled Components
 import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
 import { deleteStudent } from '../../../store/apps/Student/asyncthunk'
-import { formatCurrency,  formatDateToReadableFormat,  } from '../../../@core/utils/format'
+import { formatCurrency, formatDateToReadableFormat } from '../../../@core/utils/format'
 import { fetchStudents } from '../../../store/apps/Student/asyncthunk'
 import PageHeader from '../component/PageHeader'
 import { useExpenditure } from '../../../hooks/useExpenditure'
@@ -42,17 +42,17 @@ import { fetchExpenditure } from '../../../store/apps/expenditure/asyncthunk'
 import CreateExpenditure from './CreateExpenditure'
 import EditExpenditure from './EditExpenditure'
 import PayExpenditureBalance from './PayExpenditure'
+import ViewExpenditure from './ViewExpenditure'
 
 // ** Styled component for typography
-const TypographyStyled = styled(Typography)(({theme})=> ({
-    fontSize: theme.typography.body1.fontSize,
-    color: `${theme.palette.primary.main} !important` 
+const TypographyStyled = styled(Typography)(({ theme }) => ({
+  fontSize: theme.typography.body1.fontSize,
+  color: `${theme.palette.primary.main} !important`
 }))
-
 
 // ** renders client column
 const renderClient = row => {
-    const initials = `${row.firstName} ${row.lastName}`
+  const initials = `${row.firstName} ${row.lastName}`
   if (row.profilePicture?.length) {
     return <CustomAvatar src={row.profilePicture} sx={{ mr: 2.5, width: 38, height: 38 }} />
   } else {
@@ -69,27 +69,39 @@ const renderClient = row => {
 }
 
 const defaultColumns = [
-
-    {
-        field: 'id' , 
-        headerName: 'S/N', 
-        filterable: false,
-        renderCell:(index) => <Typography component={TypographyStyled}> {index.api.getRowIndexRelativeToVisibleRows(index.row.id) + 1} </Typography>
-    },
+  {
+    field: 'id',
+    headerName: 'S/N',
+    filterable: false,
+    renderCell: index => (
+      <Typography component={TypographyStyled}>
+        {' '}
+        {index.api.getRowIndexRelativeToVisibleRows(index.row.id) + 1}{' '}
+      </Typography>
+    )
+  },
 
   {
     flex: 0.1,
     minWidth: 100,
     field: 'amount',
     headerName: 'Amount',
-    renderCell: ({ row }) => <Typography variant='body2'  sx={{ color: 'text.secondary' }}>{formatCurrency(row.amount) || '--'}</Typography>
+    renderCell: ({ row }) => (
+      <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+        {formatCurrency(row.amount) || '--'}
+      </Typography>
+    )
   },
   {
     flex: 0.1,
     minWidth: 100,
     field: 'amountPaid',
     headerName: 'Amount Paid',
-    renderCell: ({ row }) => <Typography variant='body2'  sx={{ color: 'text.secondary' }}>{formatCurrency(row.amountPaid) || '--'}</Typography>
+    renderCell: ({ row }) => (
+      <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+        {formatCurrency(row.amountPaid) || '--'}
+      </Typography>
+    )
   },
   {
     flex: 0.15,
@@ -103,59 +115,60 @@ const defaultColumns = [
     minWidth: 140,
     field: 'createdAt',
     headerName: 'Payment Date',
-    renderCell: ({ row }) => <Typography sx={{ color: 'text.secondary' }}>{formatDateToReadableFormat(row.createdAt)}</Typography>
-  },
+    renderCell: ({ row }) => (
+      <Typography sx={{ color: 'text.secondary' }}>{formatDateToReadableFormat(row.createdAt)}</Typography>
+    )
+  }
 
+  //   {
+  //     flex: 0.1,
+  //     minWidth: 100,
+  //     field: 'balance',
+  //     headerName: 'Balance',
+  //     renderCell: ({ row }) => {
+  //       return row.balance !== 0 ? (
+  //         <Typography sx={{ color: 'text.secondary' }}>{row.balance}</Typography>
+  //       ) : (
+  //         <CustomChip rounded size='small' skin='light' color='success' label='Paid' />
+  //       )
+  //     }
+  //   },
+  //   {
+  //     flex: 0.1,
+  //     minWidth: 80,
+  //     field: 'invoiceStatus',
+  //     renderHeader: () => <Icon icon='tabler:trending-up' />,
+  //     renderCell: ({ row }) => {
+  //       const { dueDate, balance, invoiceStatus } = row
+  //       const color = invoiceStatusObj[invoiceStatus] ? invoiceStatusObj[invoiceStatus].color : 'primary'
 
-//   {
-//     flex: 0.1,
-//     minWidth: 100,
-//     field: 'balance',
-//     headerName: 'Balance',
-//     renderCell: ({ row }) => {
-//       return row.balance !== 0 ? (
-//         <Typography sx={{ color: 'text.secondary' }}>{row.balance}</Typography>
-//       ) : (
-//         <CustomChip rounded size='small' skin='light' color='success' label='Paid' />
-//       )
-//     }
-//   },
-//   {
-//     flex: 0.1,
-//     minWidth: 80,
-//     field: 'invoiceStatus',
-//     renderHeader: () => <Icon icon='tabler:trending-up' />,
-//     renderCell: ({ row }) => {
-//       const { dueDate, balance, invoiceStatus } = row
-//       const color = invoiceStatusObj[invoiceStatus] ? invoiceStatusObj[invoiceStatus].color : 'primary'
-
-//       return (
-//         <Tooltip
-//           title={
-//             <div>
-//               <Typography variant='caption' sx={{ color: 'common.white', fontWeight: 600 }}>
-//                 {invoiceStatus}
-//               </Typography>
-//               <br />
-//               <Typography variant='caption' sx={{ color: 'common.white', fontWeight: 600 }}>
-//                 Balance:
-//               </Typography>{' '}
-//               {balance}
-//               <br />
-//               <Typography variant='caption' sx={{ color: 'common.white', fontWeight: 600 }}>
-//                 Due Date:
-//               </Typography>{' '}
-//               {dueDate}
-//             </div>
-//           }
-//         >
-//           <CustomAvatar skin='light' color={color} sx={{ width: '1.875rem', height: '1.875rem' }}>
-//             <Icon icon={invoiceStatusObj[invoiceStatus].icon} />
-//           </CustomAvatar>
-//         </Tooltip>
-//       )
-//     }
-//   }
+  //       return (
+  //         <Tooltip
+  //           title={
+  //             <div>
+  //               <Typography variant='caption' sx={{ color: 'common.white', fontWeight: 600 }}>
+  //                 {invoiceStatus}
+  //               </Typography>
+  //               <br />
+  //               <Typography variant='caption' sx={{ color: 'common.white', fontWeight: 600 }}>
+  //                 Balance:
+  //               </Typography>{' '}
+  //               {balance}
+  //               <br />
+  //               <Typography variant='caption' sx={{ color: 'common.white', fontWeight: 600 }}>
+  //                 Due Date:
+  //               </Typography>{' '}
+  //               {dueDate}
+  //             </div>
+  //           }
+  //         >
+  //           <CustomAvatar skin='light' color={color} sx={{ width: '1.875rem', height: '1.875rem' }}>
+  //             <Icon icon={invoiceStatusObj[invoiceStatus].icon} />
+  //           </CustomAvatar>
+  //         </Tooltip>
+  //       )
+  //     }
+  //   }
 ]
 /* eslint-disable */
 const CustomInput = forwardRef((props, ref) => {
@@ -181,29 +194,36 @@ const AllExpenditure = () => {
   const [selectedStudent, setSelectedStudent] = useState()
   const [expenditureToUpdate, setExpenditureToUpdate] = useState(null)
   const [expenditureToPay, setExpenditureToPay] = useState(null)
+  const [expenditureInView, setExpenditureInView] = useState(null)
   const [openPayModal, setOpenPayModal] = useState(false)
-
-  
+  const [openViewModal, setOpenViewModal] = useState(false)
 
   // ** Hooks
   const dispatch = useDispatch()
 
   const [ExpenditureData] = useExpenditure()
 
-
-
-  const toggleModal = ()=>{
+  const toggleModal = () => {
     setShowModal(!showModal)
   }
 
-  const updateFetch = ()=> setFetch(!refetch)
+  const toggleViewModal = () => {
+      setOpenViewModal(!openViewModal)
+    }
+    
+    const setExpenditureToView = value => {
+      setOpenViewModal(true)
+    setExpenditureInView(value)
+  }
 
-  const setPayExpenditure = (value) => {
+  const updateFetch = () => setFetch(!refetch)
+
+  const setPayExpenditure = value => {
     setExpenditureToPay(value)
     setOpenPayModal(true)
   }
 
-  const togglePayModal = ()=> setOpenPayModal(!openPayModal)
+  const togglePayModal = () => setOpenPayModal(!openPayModal)
 
   const doDelete = value => {
     setDeleteModal(true)
@@ -216,29 +236,26 @@ const AllExpenditure = () => {
   }
 
   const ondeleteClick = async () => {
-    deleteStudent(selectedStudent).then((res)=>{
-
-        if (res.status) {
-            dispatch(fetchStudents({page: 1, key}))
-          doCancelDelete()
-        }
+    deleteStudent(selectedStudent).then(res => {
+      if (res.status) {
+        dispatch(fetchStudents({ page: 1, key }))
+        doCancelDelete()
+      }
     })
-   
   }
 
-  const setExpenditureToEdit = (value) => {
+  const setExpenditureToEdit = value => {
     setEditDrawer(true)
     setExpenditureToUpdate(value)
   }
 
-  const closeEditModal = ()=> setEditDrawer(!openEditDrawer)
+  const closeEditModal = () => setEditDrawer(!openEditDrawer)
 
-
-  useEffect(()=>{
-    dispatch(fetchExpenditure({page: page + 1, key}))
+  useEffect(() => {
+    dispatch(fetchExpenditure({ page: page + 1, key }))
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[refetch, page, key])
+  }, [refetch, page, key])
 
   const columns = [
     ...defaultColumns,
@@ -250,18 +267,24 @@ const AllExpenditure = () => {
       headerName: 'Actions',
       renderCell: ({ row }) => (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Tooltip title='Edit Expenditure'>
-             <IconButton size='small' onClick={() => setExpenditureToEdit(row)}>
-            <Icon icon='tabler:edit' />
+          <Tooltip title='Edit Expenditure'>
+            <IconButton size='small' onClick={() => setExpenditureToEdit(row)}>
+              <Icon icon='tabler:edit' />
             </IconButton>
-            </Tooltip>
-            {row.amount !== row.amountPaid && 
-            <Tooltip title='Pay Outstanding'>
-           <IconButton size='small' onClick={() => setPayExpenditure(row)}>
-                          <Icon icon='ph:hand-coins-light' />
-                        </IconButton>
           </Tooltip>
-          }
+          {row.amount !== row.amountPaid && (
+            <Tooltip title='Pay Outstanding'>
+              <IconButton size='small' onClick={() => setPayExpenditure(row)}>
+                <Icon icon='ph:hand-coins-light' />
+              </IconButton>
+            </Tooltip>
+          )}
+
+          <Tooltip title='View Expenditure'>
+            <IconButton size='small' onClick={() => setExpenditureToView(row)}>
+              <Icon icon='tabler:eye' />
+            </IconButton>
+          </Tooltip>
 
           {/* <Tooltip title='Delete Expenditure'>
             <IconButton size='small' sx={{ color: 'text.secondary' }} onClick={() => doDelete(row)}>
@@ -305,40 +328,49 @@ const AllExpenditure = () => {
 
   return (
     <Fragment>
-    <DatePickerWrapper>
-
-    {/* <Stats data={ExpenditureData} statTitle={'Students'}/> */}
+      <DatePickerWrapper>
+        {/* <Stats data={ExpenditureData} statTitle={'Students'}/> */}
 
         <PageHeader action={'Create Expenditure'} toggle={toggleModal} />
 
-      <Grid container spacing={6}>
-        <Grid item xs={12}>
-          <Card>
-            <DataGrid
-              autoHeight
-              pagination
-              rowHeight={62}
-              rows={ExpenditureData?.length ? ExpenditureData : []}
-              columns={columns}
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <DataGrid
+                autoHeight
+                pagination
+                rowHeight={62}
+                rows={ExpenditureData?.length ? ExpenditureData : []}
+                columns={columns}
 
-            //   disableRowSelectionOnClick
-              pageSizeOptions={[10, 25, 50]}
-              paginationModel={paginationModel}
-              onPaginationModelChange={setPaginationModel}
-              disableRowSelectionOnClick
-            />
-          </Card>
+                //   disableRowSelectionOnClick
+                pageSizeOptions={[10, 25, 50]}
+                paginationModel={paginationModel}
+                onPaginationModelChange={setPaginationModel}
+                disableRowSelectionOnClick
+              />
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+      </DatePickerWrapper>
 
-    </DatePickerWrapper>
+      {/* <DeleteDialog open={openDeleteModal} handleClose={doCancelDelete} handleDelete={ondeleteClick} /> */}
+      {/* <AddStudent open={showModal} closeModal={toggleModal} refetchData={updateFetch}  /> */}
+      <CreateExpenditure open={showModal} closeModal={toggleModal} fetchData={updateFetch} />
+      <EditExpenditure
+        open={openEditDrawer}
+        closeModal={closeEditModal}
+        fetchData={updateFetch}
+        selectedExpenditure={expenditureToUpdate}
+      />
+      <PayExpenditureBalance
+        expenditure={expenditureToPay}
+        open={openPayModal}
+        togglePayModal={togglePayModal}
+        fetchData={updateFetch}
+      />
 
-    {/* <DeleteDialog open={openDeleteModal} handleClose={doCancelDelete} handleDelete={ondeleteClick} /> */}
-    {/* <AddStudent open={showModal} closeModal={toggleModal} refetchData={updateFetch}  /> */}
-    <CreateExpenditure open={showModal} closeModal={toggleModal} fetchData={updateFetch}  />
-    <EditExpenditure open={openEditDrawer} closeModal={closeEditModal} fetchData={updateFetch} selectedExpenditure={expenditureToUpdate} />
-    <PayExpenditureBalance expenditure={expenditureToPay} open={openPayModal} togglePayModal={togglePayModal} fetchData={updateFetch} />
-   
+      { openViewModal && <ViewExpenditure open={openViewModal} closeCanvas={toggleViewModal} expenditure={expenditureInView}/> }
     </Fragment>
   )
 }
---
+++ src/views/users/expenditure/ViewExpenditure.jsx
@@ -0,0 +1,316 @@
+import React, { Fragment } from 'react'
+import Drawer from '@mui/material/Drawer'
+
+import IconButton from '@mui/material/IconButton'
+
+import Grid from '@mui/material/Grid'
+import Table from '@mui/material/Table'
+import Divider from '@mui/material/Divider'
+import TableRow from '@mui/material/TableRow'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import Typography from '@mui/material/Typography'
+import Box, { BoxProps } from '@mui/material/Box'
+import CardContent from '@mui/material/CardContent'
+import { styled, useTheme } from '@mui/material/styles'
+import TableContainer from '@mui/material/TableContainer'
+import TableCell, { TableCellBaseProps } from '@mui/material/TableCell'
+
+import CustomChip from 'src/@core/components/mui/chip'
+
+import Icon from 'src/@core/components/icon'
+import { Alert, Stack } from '@mui/material'
+import { formatCurrency, formatDate } from 'src/@core/utils/format'
+import { formatDateToReadableFormat } from '../../../@core/utils/format'
+
+
+const CalcWrapper = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  '&:not(:last-of-type)': {
+    marginBottom: theme.spacing(2)
+  }
+}))
+
+const Header = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(6),
+  justifyContent: 'space-between'
+}))
+
+const ViewExpenditure = ({ open, closeCanvas, expenditure }) => {
+
+
+  const theme = useTheme()
+
+  return (
+    <Drawer
+      open={open}
+      anchor='right'
+      variant='temporary'
+      ModalProps={{ keepMounted: true }}
+      sx={{ '& .MuiDrawer-paper': { width: { xs: 800, sm: 800 } } }}
+    >
+      <Header>
+        <Typography variant='h5'>View Expenditure</Typography>
+        <IconButton
+          size='small'
+          onClick={closeCanvas}
+          sx={{
+            // p: '0.438rem',
+            borderRadius: 1,
+            color: 'text.primary',
+            backgroundColor: 'action.selected',
+            '&:hover': {
+              backgroundColor: theme => `rgba(${theme.palette.customColors.main}, 0.16)`
+            }
+          }}
+        >
+          <Icon icon='tabler:x' fontSize='1.125rem' />
+        </IconButton>
+      </Header>
+
+      {/* body */}
+
+      <Box sx={{ p: theme => theme.spacing(0, 3, 3) }}>
+      
+          <Fragment>
+            {expenditure !== null && expenditure !== undefined ? (
+              <Fragment>
+                <CardContent>
+                  <Grid container sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: { xs: 'flex-start', sm: 'flex-end' },
+                        gap: 3,
+                        alignSelf: 'center'
+                      }}
+                    >
+                      {/* <Typography variant='h5'>{`#${expenditure.code}`}</Typography> */}
+
+                      {/* {expenditure.transactions[0].transactionStatus === 0 ? (
+                        <CustomChip
+                          rounded
+                          skin='light'
+                          size='small'
+                          label='Paid'
+                          color='success'
+                          sx={{ textTransform: 'capitalize' }}
+                        />
+                      ) : (
+                        <CustomChip
+                          rounded
+                          skin='light'
+                          size='small'
+                          label='Unpaid'
+                          color='error'
+                          sx={{ textTransform: 'capitalize' }}
+                        />
+                      )} */}
+                    </Box>
+
+                    <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+                      <Icon icon='healthicons:money-bag-outline' fontSize={40} />
+                      <Typography variant='h5' sx={{ alignSelf: 'center' }}>
+                        {formatCurrency(expenditure.transactions[0].amount, true)}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                </CardContent>
+
+                <Divider>Transaction Details</Divider>
+
+                <CardContent sx={{ p: [`${theme.spacing(6)} !important`, `${theme.spacing(10)} !important`] }}>
+                  <Stack spacing={4} sx={{ py: `${theme.spacing(0)} !important` }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                      <Typography sx={{ color: 'text.secondary' }}>Category:</Typography>
+
+                      <CustomChip
+                        rounded
+                        skin='light'
+                        size='small'
+                        label={expenditure.category.name}
+                        color='info'
+                        sx={{ textTransform: 'capitalize' }}
+                      />
+                    </Box>
+
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                      <Typography sx={{ color: 'text.secondary' }}>Transaction Date:</Typography>
+
+                      <Typography sx={{ color: 'text.secondary' }}>
+                        {formatDateToReadableFormat(expenditure.createdAt)}
+                      </Typography>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                      <Typography sx={{ color: 'text.secondary' }}>Status:</Typography>
+
+                      {expenditure.transactions[0].transactionStatus === 0 ? (
+                        <CustomChip
+                          rounded
+                          skin='light'
+                          size='small'
+                          label='Paid'
+                          color='success'
+                          sx={{ textTransform: 'capitalize' }}
+                        />
+                      ) : (
+                        <CustomChip
+                          rounded
+                          skin='light'
+                          size='small'
+                          label='Unpaid'
+                          color='error'
+                          sx={{ textTransform: 'capitalize' }}
+                        />
+                      )}
+                    </Box>
+                  </Stack>
+                </CardContent>
+
+                {/* 
+
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Item</TableCell>
+                        <TableCell>qty</TableCell>
+                        <TableCell>Unit Price</TableCell>
+                        <TableCell>Sub Total</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody
+                      sx={{
+                        '& .MuiTableCell-root': {
+                          py: `${theme.spacing(2.5)} !important`,
+                          fontSize: theme.typography.body1.fontSize
+                        }
+                      }}
+                    >
+                      {expenditure.items.map(item => (
+                        <TableRow key={item.id}>
+                          <TableCell>{item.name}</TableCell>
+
+                          <TableCell>{item.quantity}</TableCell>
+                          <TableCell>{formatCurrency(item.unitPrice, true)}</TableCell>
+                          <TableCell>{formatCurrency(Math.abs(item.quantity * item.unitPrice), true)}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer> */}
+
+                <Divider sx={{mt: '10px', mb: '10px'}}>Transaction History</Divider>
+
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>S/N</TableCell>
+                        <TableCell>AMOUNT</TableCell>
+                        <TableCell>AMOUNT PAID</TableCell>
+                        <TableCell>DESCRIPTION</TableCell>
+                        <TableCell>DATE</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody
+                      sx={{
+                        '& .MuiTableCell-root': {
+                          py: `${theme.spacing(2.5)} !important`,
+                          fontSize: theme.typography.body1.fontSize
+                        }
+                      }}
+                    >
+                      {expenditure.transactions.map((item, i) => (
+                        <TableRow key={item.id}>
+                            <TableCell>{i + 1}</TableCell>
+
+
+                          <TableCell>{formatCurrency(item?.amount, true) || '--'}</TableCell>
+
+                          <TableCell>{formatCurrency(item.amountPaid, true)}</TableCell>
+                          <TableCell>{item?.description}</TableCell>
+                          <TableCell>
+                            {formatDateToReadableFormat(item.createdAt)}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+
+                <CardContent sx={{ p: [`${theme.spacing(6)} !important`, `${theme.spacing(10)} !important`] }}>
+                  <Grid container>
+                    <Grid item xs={12} sm={7} lg={9} sx={{ order: { sm: 1, xs: 2 } }}>
+                      <Box sx={{ mb: 2, display: 'flex', alignItems: 'center' }}>
+                        <Typography sx={{ mr: 2, fontWeight: 500, color: 'text.secondary' }}>Payee:</Typography>
+                        <Typography sx={{ color: 'text.secondary' }}>{`${expenditure?.staff?.firstName?.toUpperCase()} ${expenditure?.staff?.lastName?.toUpperCase()}`}</Typography>
+                      </Box>
+
+                      {/* <Typography sx={{ color: 'text.secondary' }}></Typography> */}
+                    </Grid>
+                    <Grid item xs={12} sm={5} lg={3} sx={{ mb: { sm: 0, xs: 4 }, order: { sm: 2, xs: 1 } }}>
+                      <CalcWrapper>
+                        <Typography sx={{ color: 'text.secondary' }}>Amount:</Typography>
+                        <Typography sx={{ fontWeight: 500, color: 'text.secondary' }}>
+                          {formatCurrency(expenditure?.amount, true)}
+                        </Typography>
+                      </CalcWrapper>
+                      <CalcWrapper>
+                        <Typography sx={{ color: 'text.secondary' }}>Amount Paid:</Typography>
+                        <Typography sx={{ fontWeight: 500, color: 'text.secondary' }}>
+                          {formatCurrency(expenditure?.amountPaid, true)}
+                        </Typography>
+                      </CalcWrapper>
+
+                      <CalcWrapper>
+                        <Typography sx={{ color: 'text.secondary' }}>Outstanding Amount:</Typography>
+                        <Typography sx={{ fontWeight: 500, color: 'text.secondary' }}>
+                          {formatCurrency((Number(expenditure?.amount) - Number(expenditure?.amountPaid)), true)}
+                        </Typography>
+                      </CalcWrapper>
+                      {/* <CalcWrapper sx={{ mb: '0 !important' }}>
+                        <Typography sx={{ color: 'text.secondary' }}>Tax:</Typography>
+                        <Typography sx={{ fontWeight: 500, color: 'text.secondary' }}>21%</Typography>
+                      </CalcWrapper> */}
+                      <Divider sx={{ my: `${theme.spacing(2)} !important` }} />
+                      {/* <CalcWrapper>
+                        <Typography sx={{ color: 'text.secondary' }}>Total:</Typography>
+                        <Typography sx={{ fontWeight: 500, color: 'text.secondary' }}>
+                          {formatCurrency(Math.abs(expenditure.transactions[0].amount), true)}
+                        </Typography>
+                      </CalcWrapper> */}
+                    </Grid>
+                  </Grid>
+                </CardContent>
+
+                <Divider />
+
+                <CardContent sx={{ px: [6, 10] }}>
+                  <Typography sx={{ color: 'text.secondary' }}>
+                    <Typography component='span' sx={{ mr: 1.5, fontWeight: 500, color: 'inherit' }}>
+                      Title:
+                    </Typography>
+                    {expenditure.title || 'No available note'}
+                  </Typography>
+                </CardContent>
+              </Fragment>
+            ) : (
+              <Grid container spacing={6}>
+                <Grid item xs={12}>
+                  <Alert severity='error'>Expenditure does not exist. Please check the list of Expenditures: </Alert>
+                </Grid>
+              </Grid>
+            )}
+          </Fragment>
+      </Box>
+    </Drawer>
+  )
+}
+
+export default ViewExpenditure
