d3c0459e087d2bf392ce1a6a2b75af6907724abc
blessme247 <solabiabraham@gmail.com>
2024-02-22 13:01:59 +0100

    Added category tab to subjects page

---
+++ src/views/users/subjects/ManageCategories.jsx
@@ -0,0 +1,183 @@
+// ** React Imports
+import { useEffect, useState } from 'react'
+
+// ** MUI Imports
+import Drawer from '@mui/material/Drawer'
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+
+// ** Custom Component Import
+import CustomTextField from 'src/@core/components/mui/text-field'
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm, Controller } from 'react-hook-form'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+import { useAppDispatch } from 'src/hooks'
+import { CircularProgress } from '@mui/material'
+import { createCategory, fetchCategories, updateCategory } from '../../../store/apps/categories/asyncthunk'
+
+
+
+const showErrors = (field, valueLen, min) => {
+  if (valueLen === 0) {
+    return `${field} field is required`
+  } else if (valueLen > 0 && valueLen < min) {
+    return `${field} must be at least ${min} characters`
+  } else {
+    return ''
+  }
+}
+
+const Header = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(6),
+  justifyContent: 'space-between'
+}))
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .min(3, obj => showErrors('name', obj.value.length, obj.min))
+    .required()
+})
+
+const defaultValues = {
+  name: '',
+  type: 'subject'
+}
+
+const ManageCategories = ({ open, toggle, categoryToEdit = null }) => {
+  const dispatch = useAppDispatch()
+
+  const {
+    reset,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm({
+    defaultValues,
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  })
+
+  const handleClose = () => {
+    toggle()
+    reset()
+  }
+
+  const onSubmit = async (data) => {
+
+    const payload = {
+        name: data.name,
+        type: defaultValues.type
+      }
+
+      createCategory(payload).then((response)=>{
+          if (response.data.success){
+            handleClose()
+            dispatch(fetchCategories({ page: 1, limit: 10, type: 'subject' }))
+          }
+      })
+
+  }
+
+  const onUpdate = async (data) => {
+    
+      const payload = {
+        name: data.name,
+        type: defaultValues.type
+      }
+
+
+      updateCategory(categoryToEdit?.id, payload).then((response)=>{
+        if (response.data.success){
+            handleClose()
+            dispatch(fetchCategories({ page: 1, limit: 10, type: 'subject' }))
+          }
+      })
+
+     
+
+
+  }
+
+  useEffect(() => {
+    if (categoryToEdit !== null) {
+      setValue('name', categoryToEdit.name)
+      setValue('type', categoryToEdit.type)
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <Drawer
+      open={open}
+      anchor='right'
+      variant='temporary'
+      ModalProps={{ keepMounted: true }}
+      sx={{ '& .MuiDrawer-paper': { width: { xs: 300, sm: 400 } } }}
+    >
+      <Header>
+        <Typography variant='h5'>{categoryToEdit ? 'Edit Category' : 'Create Category'}</Typography>
+        <IconButton
+          size='small'
+          onClick={handleClose}
+          sx={{
+            p: '0.438rem',
+            borderRadius: 1,
+            color: 'text.primary',
+            backgroundColor: 'action.selected',
+            '&:hover': {
+              backgroundColor: theme => `rgba(${theme.palette.customColors.main}, 0.16)`
+            }
+          }}
+        >
+          <Icon icon='tabler:x' fontSize='1.125rem' />
+        </IconButton>
+      </Header>
+      <Box sx={{ p: theme => theme.spacing(0, 6, 6) }}>
+        <form onSubmit={handleSubmit(categoryToEdit ? onUpdate : onSubmit)}>
+
+
+          <Controller
+            name='name'
+            control={control}
+            rules={{ required: true }}
+            render={({ field: { value, onChange } }) => (
+              <CustomTextField
+                fullWidth
+                value={value}
+                sx={{ mb: 4 }}
+                label='Category Name'
+                required
+                onChange={onChange}
+                placeholder='Category Name'
+                error={Boolean(errors.name)}
+                {...(errors.name && { helperText: errors.name.message })}
+              />
+            )}
+          />
+
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Button type='submit' variant='contained' sx={{ width: '100%' }}>
+              {isSubmitting && <CircularProgress size={20} color='secondary' sx={{ ml: 2 }} />}
+              {categoryToEdit ? 'Update' : 'Create'}
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Drawer>
+  )
+}
+
+export default ManageCategories
