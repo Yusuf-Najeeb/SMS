2f4b69b6e8a4eee48ba5c4e05ce59c5d388333a3
blessme247 <solabiabraham@gmail.com>
2024-03-09 09:32:00 +0100

    Fix: ESLint Build issues

--- src/views/users/Student/StudentCard.jsx
+++ src/views/users/Student/StudentCard.jsx
@@ -73,7 +73,9 @@ const StudentCard = ({ Student }) => {
       setStudentClass({...classInfo})
     }
   }
+
    // Cleanup function
+   
    return () => {
     isMounted = false;
     setStudentClass({});
