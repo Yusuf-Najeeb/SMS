2e13a00ae0d10342227ba9d34eaceec2535b1f50
Muhammad Najeeb <yusnaj21@gmail.com>
2024-03-14 13:58:01 +0100

    Modified: pagination feature on expenditure page

--- src/store/apps/expenditure/index.js
+++ src/store/apps/expenditure/index.js
@@ -6,12 +6,12 @@ const initialState = {
   loading: false,
   ExpenditureData: [],
 
-//   paging: {
-//     currentPage: 1,
-//     totalItems: 0,
-//     itemsPerPage: 0,
-//     totalPages: 0
-//   }
+  paging: {
+    currentPage: 1,
+    totalItems: 0,
+    itemsPerPage: 0,
+    totalPages: 0
+  }
 }
 
 export const expenditureSlice = createSlice({
@@ -23,11 +23,9 @@ export const expenditureSlice = createSlice({
       state.loading = true
     })
     builder.addCase(fetchExpenditure.fulfilled, (state, action) => {
-
       state.loading = false
       state.ExpenditureData = action?.payload?.data.data
-      
-    //   state.paging = action?.payload?.data?.paging
+      state.paging = action?.payload?.data?.paging
     })
     builder.addCase(fetchExpenditure.rejected, state => {
       state.loading = false
